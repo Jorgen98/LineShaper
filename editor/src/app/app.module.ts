9e00d5b1ec1a1eeac61f26b48356ab826d7804f3
Juraj Lazúr <juraj51112@gmail.com>
2024-07-31 15:18:24 +0200

    angular update 6

--- editor/src/app/app.module.ts
+++ editor/src/app/app.module.ts
@@ -28,7 +28,6 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     bootstrap: [AppComponent], imports: [CommonModule,
         BrowserModule,
         MatIconModule,
-        HttpClientModule,
         FormsModule,
         TranslateModule.forRoot({
             defaultLanguage: 'cz',
